4ee28d73f542d1437a5e3a4cbe98d245d04e7883
Vijayvilam <38938373+Vijayvilam@users.noreply.github.com>
2018-09-14 16:37:40 +0530

    Unit Test file

---
+++ TestRetailStore.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRetailStore {
+Object Pur;
+	@Test
+	  public class UnitTest1
+	   {
+	      public void TestMethod1()
+			        {
+		                Pur = new Purchase();
+			            Pur.discount();
+			            Pur.GenerateBill();
+			        }
+			    }
+
+}
